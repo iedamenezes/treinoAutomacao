099e57d89962dab1b342b514eb28b9e3a17f450d
iedamenezes <ieda_menezes@hotmail.com>
2023-04-20 17:14:27 -0300

    First Commit

---
+++ src/main/java/br/com/chronosacademy/Media/Media.java
@@ -0,0 +1,11 @@
+package br.com.chronosacademy.Media;
+
+public class Media {
+    public String resultadoMedia (double n1, double n2){
+        double media = (n1+n2)/2;
+        if(media <5){
+            return "Reprovado";
+        }
+        return "Aprovado";
+    }
+}
